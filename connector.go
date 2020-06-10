184a903db9bea4cfb0873ec71c3aaf551fd75c19
Matt Davidson <matt_davidson@alliedtelesis.com>
2020-06-10 11:56:53 -0400

    Update

--- connector.go
+++ connector.go
@@ -6,25 +6,6 @@ import (
 	"time"
 )
 
-// EspMessage contains the topic and payload
-type ConnectorMessage struct {
-	Entity   string
-	Snapshot MeasurementSnapshot
-}
-
-// EspPayload is part of the payload data.
-type MeasurementSnapshot struct {
-	Datetime     string        `json:"datetime"`
-	Measurements []Measurement `json:"values"`
-}
-
-// EspValues contains the data for each measurement
-type Measurement struct {
-	Field      string  `json:"field"`
-	Amount     float64 `json:"amount"`
-	Attributes string  `json:"attributes"`
-}
-
 func after(target string, after string) string {
 	// Get substring after a string.
 	pos := strings.LastIndex(target, after)
@@ -63,10 +44,12 @@ func FormatESPTime(timestring string) string {
 
 	// Remove the T and/or _ between the date and time
 	timeStr := strings.Replace(timestring, "T", " ", 1)
-	timeStr = strings.Replace(timestring, "_", " ", 1)
-	fmt.Println(timeStr)
+	fmt.Println("timeStr ", timeStr)
+	timeStr = strings.Replace(timeStr, "_", " ", 1)
+	fmt.Println("timeStr ", timeStr)
 	// Add the time zone.
 	datetime := timeStr + " " + timeZone
+	fmt.Println("datetime ", datetime)
 	// Convert from current location
 	t, err := time.Parse("2006-01-02 15:04:05.000 MST", datetime)
 	if err != nil {
@@ -80,39 +63,4 @@ func FormatESPTime(timestring string) string {
 	// Convert to ESP format.
 	utcTimeStr := utcTime.Format("2006-01-02T15:04:05.000Z")
 	return utcTimeStr
-}
-
-// Decode decodes the message
-func Decode(connectorMessage map[string]interface{}) (string, map[string]interface{}) {
-
-	snapshotMap := make(map[string]interface{})
-
-	msg := connectorMessage["msg"]
-	msg1 := msg.(ConnectorMessage)
-	snapshot := msg1.Snapshot
-	snapshotMap["datetime"] = snapshot.Datetime
-
-	var valuesSlice []interface{}
-	for _, value := range snapshot.Measurements {
-		field := value.Field
-		amount := value.Amount
-		attr := value.Attributes
-		fmt.Println("\nattr:\n", attr)
-		fmt.Printf("%T\n", attr)
-		if value.Attributes == "" {
-			value := map[string]interface{}{
-				"field":  field,
-				"amount": amount,
-			}
-			valuesSlice = append(valuesSlice, value)
-		} else {
-			value := map[string]interface{}{
-				"field":     field,
-				"amount":    amount,
-				"attribute": attr}
-			valuesSlice = append(valuesSlice, value)
-		}
-	}
-	snapshotMap["values"] = valuesSlice
-	return msg1.Entity, snapshotMap
 }
